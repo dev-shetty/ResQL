75a3eb45a7e77951b1d806eb1977f057c7335d54
Rohan <rohankumar5803@gmail.com>
2024-03-16 22:27:38 +0530

    updated frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,12 @@
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom"
-import "./App.css"
-import DisasterRegForm from "./Pages/DisasterRegForm"
-import DonationForm from "./Pages/DonationForm"
-import DonationPage from "./Pages/Donations"
-import Home from "./Pages/Home"
-import Login from "./Pages/Login"
-import RescuerRegForm from "./Pages/RescuerRegForm"
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import "./App.css";
+import DisasterRegForm from "./Pages/DisasterRegForm";
+import DonationForm from "./Pages/DonationForm";
+import DonationPage from "./Pages/Donations";
+import Home from "./Pages/Home";
+import Login from "./Pages/Login";
+import RescuerRegForm from "./Pages/RescuerRegForm";
+import Profile from "./Pages/Profile";
 
 function App() {
   return (
@@ -17,9 +18,10 @@ function App() {
         <Route path="/rescuer/register" element={<RescuerRegForm />} />
         <Route path="/donate" element={<DonationForm />} />
         <Route path="/donations" element={<DonationPage />} />
+        <Route path="/profile" element={<Profile />} />
       </Routes>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
--- client/src/Components/DonateAndRescue.jsx
+++ client/src/Components/DonateAndRescue.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import "../styles/DonateAndRescue.css";
 import { Link } from "react-router-dom";
+import { useAuth } from "../context/auth-provider";
 
 function DonateAndRescue() {
+  const { auth } = useAuth();
   return (
     <div className="donate-and-rescue-section">
       <div className="donate-section">
@@ -12,13 +14,15 @@ function DonateAndRescue() {
           <button className="button">Contribute for a cause</button>
         </Link>
       </div>
-      <div className="rescuer-registration-section">
-        <p>Join us in the mission</p>
-        <p>Register as a Rescuer!</p>
-        <Link to="/rescuer/register">
-          <button className="button">Register as a Rescuer</button>
-        </Link>
-      </div>
+      {auth == null && (
+        <div className="rescuer-registration-section">
+          <p>Join us in the mission</p>
+          <p>Register as a Rescuer!</p>
+          <Link to="/rescuer/register">
+            <button className="button">Register as a Rescuer</button>
+          </Link>
+        </div>
+      )}
     </div>
   );
 }
